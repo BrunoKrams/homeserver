33ae91c2c148c74bfc3f7379260189eb70380594
markus <mark.born@web.de>
2023-04-12 13:30:23 +0200

    WIP

--- main/business/lightswitch/raspberry_lightswitch_adapter.py
+++ main/business/lightswitch/raspberry_lightswitch_adapter.py
@@ -1,7 +1,6 @@
-import time
 from abc import abstractmethod, ABC
 
-import RPi.GPIO as GPIO
+from main.business.lightswitch.logic import LightswitchAdapter
 
 
 class GpioPin(ABC):
@@ -19,26 +18,7 @@ class GpioPin(ABC):
         pass
 
 
-class GpioPinImpl(GpioPin):
-
-    def __init__(self, pin: int):
-        self.pin = pin
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(pin, GPIO.OUT)
-
-    def __del__(self):
-        GPIO.cleanup()
-
-    def status(self) -> int:
-        return GPIO.input(self.pin)
-
-    def high(self):
-        GPIO.output(self.pin, GPIO.HIGH)
-
-    def low(self):
-        GPIO.output(self.pin, GPIO.LOW)
-
-class RaspberryLightswitchAdapter():
+class RaspberryLightswitchAdapter(LightswitchAdapter):
 
     def __init__(self, gpio_pin: GpioPin):
         self.gpio_pin = gpio_pin
@@ -52,11 +32,4 @@ class RaspberryLightswitchAdapter():
     def off(self):
         self.gpio_pin.low()
 
-if __name__ == '__main__':
-    gpio_pin = GpioPinImpl(18)
-    lightswitch_adapter = RaspberryLightswitchAdapter(gpio_pin)
-    lightswitch_adapter.on()
-    print('ON' if lightswitch_adapter.status() else "OFF")
-    time.sleep(4)
-    lightswitch_adapter.off()
-    print('ON' if lightswitch_adapter.status() else "OFF")
+
